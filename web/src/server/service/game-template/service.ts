be91de1ccd98c965065fd969e596e31662dd0a78
Will Redington <will86325@gmail.com>
2024-08-07 08:19:37 -0400

    stuff

--- web/src/server/service/game-template/service.ts
+++
@@ -1,3 +0,0 @@
-export async function createGameRoomTemplate() {
-  // todo
-}
--- web/src/server/service/player/util.ts
+++
@@ -1,8 +0,0 @@
-import { randomBytes } from "crypto";
-
-export const getPlayerSessionStoreKey = ({ token }: { token: string }) =>
-  `player:${token}`;
-
-export function generateToken() {
-  return randomBytes(16).toString("hex");
-}
